4a2e33c2fdbd3c1a3feca0a86c1e7fe45998b2a7
Mustafa Elbaz <m9stafa05@gmail.com>
2025-11-12 19:18:50 +0200

    Build  LoginScreen Ui layout and add CustomTextFormField widget

--- lib/core/themes/app_colors.dart
+++ lib/core/themes/app_colors.dart
@@ -2,6 +2,6 @@ import 'package:flutter/material.dart';
 
 class AppColors {
   static const darkBlue = Color(0xFF101922);
-  static const grayColor = Color(0xFF9CA3AF);
+  static const grayColor = Color(0xFFA1A1AA);
   static const lightBlue = Color(0xFF1585F4);
 }
