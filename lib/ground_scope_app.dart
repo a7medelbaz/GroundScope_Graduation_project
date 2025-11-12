7ff9eb73f6bea92f14fedee037b6fa9aacb5bad3
Mustafa Elbaz <m9stafa05@gmail.com>
2025-11-12 02:51:10 +0200

    Add OnBoardingScreen and CustomTextButton components; update GroundScopeApp for initial routing

--- lib/ground_scope_app.dart
+++ lib/ground_scope_app.dart
@@ -1,26 +1,32 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:ground_scope/core/router/app_routers.dart';
+import 'package:ground_scope/core/router/routes.dart';
+import 'package:ground_scope/core/themes/app_colors.dart';
 
 class GroundScopeApp extends StatelessWidget {
   const GroundScopeApp({super.key, required this.appRouter});
   final AppRouter appRouter;
+
   @override
   Widget build(BuildContext context) {
     return ScreenUtilInit(
-      // ToDo :App Main Size
-      designSize: const Size(375, 812),
+      designSize: const Size(390, 844),
       minTextAdapt: true,
-      child: MaterialApp(
-        debugShowCheckedModeBanner: false,
-        // initialRoute: Routes.onBoardingScreen,
-        onGenerateRoute: AppRouter().generateRoute,
-        title: 'GroundScope',
-        // theme: ThemeData(
-        //   primaryColor: ColorsManger.mainBlue,
-        //   scaffoldBackgroundColor: Colors.white,
-        // ),
-      ),
+      splitScreenMode: true,
+      builder: (context, child) {
+        return MaterialApp(
+          debugShowCheckedModeBanner: false,
+          initialRoute: Routes.onBoardingScreen,
+          onGenerateRoute:
+              appRouter.generateRoute,
+          title: 'GroundScope',
+          theme: ThemeData(
+            primaryColor: Colors.white,
+            scaffoldBackgroundColor: AppColors.darkBlue,
+          ),
+        );
+      },
     );
   }
 }
