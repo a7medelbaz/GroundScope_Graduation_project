ddf56ef362dd44915f68ef33cf76f6ef3bfd5c6e
Mustafa Elbaz <m9stafa05@gmail.com>
2025-11-12 02:46:51 +0200

    Build Navigation method and Add Routes file For app routes & Build extension on the context for navigation between screens

--- lib/core/router/app_routers.dart
+++ lib/core/router/app_routers.dart
@@ -1,41 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:ground_scope/core/auth/ui/login_screen.dart';
+import 'package:ground_scope/core/onboarding/ui/on_boarding_screen.dart';
+import 'package:ground_scope/core/router/routes.dart';
+
 class AppRouter {
   Route<dynamic> generateRoute(RouteSettings settings) {
     // ignore: unused_local_variable
     final arguments = settings.arguments;
 
     switch (settings.name) {
-      // case Routes.onBoardingScreen:
-      //   return MaterialPageRoute(
-      //     builder: (_) => const OnBoardingScreen(),
-      //   );
-
-      // case Routes.loginScreen:
-      //   return MaterialPageRoute(
-      //     builder: (_) => BlocProvider(
-      //       create: (context) => getIt<LoginCubit>(),
-      //       child: const LoginScreen(),
-      //     ),
-      //   );
-
-      // case Routes.signUpScreen:
-      //   return MaterialPageRoute(
-      //     builder: (_) => BlocProvider(
-      //       create: (context) => getIt<SignUpCubit>(),
-      //       child: const SignUpScreen(),
-      //     ),
-      //   );
-
-      // case Routes.homeScreen:
-      //   return MaterialPageRoute(
-      //     builder: (_) => const HomeScreen(),
-      //   );
+      case Routes.onBoardingScreen:
+        return MaterialPageRoute(builder: (_) => const OnBoardingScreen());
+      case Routes.loginScreen:
+        return MaterialPageRoute(builder: (_) => const LoginScreen());
 
       default:
         return MaterialPageRoute(
-          builder: (_) => const Scaffold(
-            body: Center(child: Text('Page not found')),
-          ),
+          builder: (_) =>
+              const Scaffold(body: Center(child: Text('Page not found'))),
         );
     }
   }
