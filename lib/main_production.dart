7b4efb68396e4994428d922c9b0a414b7591afd8
Mustafa Elbaz <m9stafa05@gmail.com>
2025-11-12 02:48:26 +0200

    Update the main by remove the init of screen util for avoiding app crash

--- lib/main_production.dart
+++ lib/main_production.dart
@@ -1,12 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:ground_scope/core/di/dependency_injection.dart';
 import 'package:ground_scope/core/router/app_routers.dart';
 import 'package:ground_scope/ground_scope_app.dart';
 
 void main() async {
-  setupGetIt();
-  await ScreenUtil.ensureScreenSize();
+  WidgetsFlutterBinding.ensureInitialized();
+  await setupGetIt();
   runApp(GroundScopeApp(appRouter: AppRouter()));
 }
 // flutter run --release --flavor production --target lib/main_production.dart
