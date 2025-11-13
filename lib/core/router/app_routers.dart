fc8f5a5950233277e970a590084f8114f3ed1c64
Ahmed_Elbaz <aelbaz0189@gmail.com>
2025-11-13 23:49:49 +0200

    editing the Approuter

--- lib/core/router/app_routers.dart
+++ lib/core/router/app_routers.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:ground_scope/modules/admin/features/home/admin_screen.dart';
+import 'package:ground_scope/modules/supervisor/features/home/supervisor_screen.dart';
 import '../auth/data/repo/auth_repo.dart';
 import '../di/dependency_injection.dart';
-
 import '../auth/logic/cubit/auth_cubit.dart';
-import '../../modules/worker/features/home/ui/widgets/home_screen.dart';
+import '../../modules/worker/features/home/ui/widgets/worker_screen.dart';
 import '../auth/ui/login_screen.dart';
 import '../onboarding/ui/on_boarding_screen.dart';
 import 'routes.dart';
@@ -26,8 +27,16 @@ class AppRouter {
             child: const LoginScreen(),
           ),
         );
-      case Routes.homeScreen:
-        return MaterialPageRoute(builder: (_) => const HomeScreen());
+      case Routes.workerScreen:
+        return MaterialPageRoute(
+          builder: (_) => const WorkerScreen(),
+        );
+      case Routes.supervisorScreen:
+        return MaterialPageRoute(
+          builder: (_) => const SupervisorScreen(),
+        );
+      case Routes.adminScreen:
+        return MaterialPageRoute(builder: (_) => const AdminScreen());
 
       default:
         return MaterialPageRoute(
