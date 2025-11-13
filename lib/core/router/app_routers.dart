3b6a454143ba7859cef033d3143e49d562f52cc3
Ahmed_Elbaz <aelbaz0189@gmail.com>
2025-11-13 19:55:52 +0200

    Add secure storage utility and update routing for login and home screens

--- lib/core/router/app_routers.dart
+++ lib/core/router/app_routers.dart
@@ -1,4 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import '../auth/data/repo/auth_repo.dart';
+import '../di/dependency_injection.dart';
+
+import '../auth/logic/cubit/auth_cubit.dart';
+import '../../modules/worker/features/home/ui/widgets/home_screen.dart';
 import '../auth/ui/login_screen.dart';
 import '../onboarding/ui/on_boarding_screen.dart';
 import 'routes.dart';
@@ -10,14 +16,24 @@ class AppRouter {
 
     switch (settings.name) {
       case Routes.onBoardingScreen:
-        return MaterialPageRoute(builder: (_) => const OnBoardingScreen());
+        return MaterialPageRoute(
+          builder: (_) => const OnBoardingScreen(),
+        );
       case Routes.loginScreen:
-        return MaterialPageRoute(builder: (_) => const LoginScreen());
+        return MaterialPageRoute(
+          builder: (_) => BlocProvider(
+            create: (context) => AuthCubit(getIt<AuthRepo>()),
+            child: const LoginScreen(),
+          ),
+        );
+      case Routes.homeScreen:
+        return MaterialPageRoute(builder: (_) => const HomeScreen());
 
       default:
         return MaterialPageRoute(
-          builder: (_) =>
-              const Scaffold(body: Center(child: Text('Page not found'))),
+          builder: (_) => const Scaffold(
+            body: Center(child: Text('Page not found')),
+          ),
         );
     }
   }
