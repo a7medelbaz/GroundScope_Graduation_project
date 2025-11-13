d5c437b34974c2bfe57c5367c2af0b5af98eef08
Ahmed_Elbaz <aelbaz0189@gmail.com>
2025-11-14 01:16:45 +0200

    Add role-based initial screen navigation in main development and production files

--- lib/main_development.dart
+++ lib/main_development.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:ground_scope/core/onboarding/ui/on_boarding_screen.dart';
+import 'package:ground_scope/core/utils/secure_storage.dart';
+import 'package:ground_scope/modules/admin/features/home/admin_screen.dart';
+import 'package:ground_scope/modules/supervisor/features/home/supervisor_screen.dart';
+import 'package:ground_scope/modules/worker/features/home/ui/widgets/worker_screen.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 import 'core/di/dependency_injection.dart';
@@ -13,7 +18,26 @@ void main() async {
         'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJtZnNvYWR1eGFzbWxwa2R5emppIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjI5MzgxMzAsImV4cCI6MjA3ODUxNDEzMH0.Y3pODH_C9L1HGEFYTj_0FNPuQaUeZntlK4ROUVk7-tA',
   );
   await setupGetIt();
-  runApp(GroundScopeApp(appRouter: AppRouter()));
+
+  final storage = SecureStorage();
+  final cachedRole = await storage.getString(key: 'position');
+
+  Widget initialScreen = const OnBoardingScreen();
+
+  if (cachedRole == 'worker') {
+    initialScreen = const WorkerScreen();
+  } else if (cachedRole == 'supervisor') {
+    initialScreen = const SupervisorScreen();
+  } else if (cachedRole == 'admin') {
+    initialScreen = const AdminScreen();
+  }
+  runApp(
+    GroundScopeApp(
+      appRouter: AppRouter(),
+      initialScreen: initialScreen,
+    ),
+  );
 }
 
 // flutter run --release --flavor development --target lib/main_development.dart
+// flutter build apk --flavor development --target lib/main_development.dart
--- lib/main_production.dart
+++ lib/main_production.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:ground_scope/core/onboarding/ui/on_boarding_screen.dart';
+import 'package:ground_scope/core/utils/secure_storage.dart';
+import 'package:ground_scope/modules/admin/features/home/admin_screen.dart';
+import 'package:ground_scope/modules/supervisor/features/home/supervisor_screen.dart';
+import 'package:ground_scope/modules/worker/features/home/ui/widgets/worker_screen.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 import 'core/di/dependency_injection.dart';
 import 'core/router/app_routers.dart';
 import 'ground_scope_app.dart';
-
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Supabase.initialize(
@@ -13,6 +17,27 @@ void main() async {
         'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJtZnNvYWR1eGFzbWxwa2R5emppIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjI5MzgxMzAsImV4cCI6MjA3ODUxNDEzMH0.Y3pODH_C9L1HGEFYTj_0FNPuQaUeZntlK4ROUVk7-tA',
   );
   await setupGetIt();
-  runApp(GroundScopeApp(appRouter: AppRouter()));
+
+  final storage = SecureStorage();
+  final cachedRole = await storage.getString(key: 'position');
+
+  Widget initialScreen = const OnBoardingScreen();
+
+  if (cachedRole == 'worker') {
+    initialScreen = const WorkerScreen();
+  } else if (cachedRole == 'supervisor') {
+    initialScreen = const SupervisorScreen();
+  } else if (cachedRole == 'admin') {
+    initialScreen = const AdminScreen();
+  }
+  runApp(
+    GroundScopeApp(
+      appRouter: AppRouter(),
+      initialScreen: initialScreen,
+    ),
+  );
 }
+
+
 // flutter run --release --flavor production --target lib/main_production.dart
+// flutter build apk --flavor production --target lib/main_production.dart
