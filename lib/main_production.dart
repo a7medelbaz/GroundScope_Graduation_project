dc3e38aa3299b69492c5fea63eaabfa7a8843a7e
Ahmed_Elbaz <aelbaz0189@gmail.com>
2025-11-13 19:54:51 +0200

    init Supabase

--- lib/main_production.dart
+++ lib/main_production.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 import 'core/di/dependency_injection.dart';
 import 'core/router/app_routers.dart';
@@ -6,6 +7,11 @@ import 'ground_scope_app.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Supabase.initialize(
+    url: 'https://bmfsoaduxasmlpkdyzji.supabase.co',
+    anonKey:
+        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJtZnNvYWR1eGFzbWxwa2R5emppIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjI5MzgxMzAsImV4cCI6MjA3ODUxNDEzMH0.Y3pODH_C9L1HGEFYTj_0FNPuQaUeZntlK4ROUVk7-tA',
+  );
   await setupGetIt();
   runApp(GroundScopeApp(appRouter: AppRouter()));
 }
