6a65a16931d5c1878c99c366b647765095190f85
mustafaelbaz5 <m9stafa05@gmail.com>
2026-02-27 17:45:16 +0200

    Set App Configs

---
+++ lib/core/config/app_config.dart
@@ -0,0 +1,10 @@
+class AppConfig {
+  AppConfig._();
+  static const String supaBaseUr = 'https://bmfsoaduxasmlpkdyzji.supabase.co';
+  static const String supaBaseKey =
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJtZnNvYWR1eGFzbWxwa2R5emppIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjI5MzgxMzAsImV4cCI6MjA3ODUxNDEzMH0.Y3pODH_C9L1HGEFYTj_0FNPuQaUeZntlK4ROUVk7-tA';
+
+  // App Version
+  static const String appVersion = '1.0.0';
+  
+}
