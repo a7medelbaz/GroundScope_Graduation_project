c87159aacbec8ae7cc412f540c95537d8966bc00
mustafaelbaz5 <m9stafa05@gmail.com>
2026-02-27 14:44:47 +0200

    Set App Constants for supa base

---
+++ lib/core/config/app_constants.dart
@@ -0,0 +1,6 @@
+class AppConstants {
+  AppConstants._();
+  static const String supaBaseUr = 'https://bmfsoaduxasmlpkdyzji.supabase.co';
+  static const String supaBaseKey =
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJtZnNvYWR1eGFzbWxwa2R5emppIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjI5MzgxMzAsImV4cCI6MjA3ODUxNDEzMH0.Y3pODH_C9L1HGEFYTj_0FNPuQaUeZntlK4ROUVk7-tA';
+}
--- lib/main_development.dart
+++ lib/main_development.dart
@@ -1,20 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:ground_scope/core/config/app_constants.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+import 'core/di/dependency_injection.dart';
 import 'core/onboarding/ui/on_boarding_screen.dart';
+import 'core/router/app_routers.dart';
 import 'core/utils/secure_storage.dart';
+import 'ground_scope_app.dart';
 import 'modules/admin/features/home/admin_screen.dart';
 import 'modules/supervisor/features/home/supervisor_screen.dart';
 import 'modules/worker/core/widgets/worker_screen.dart';
-import 'package:supabase_flutter/supabase_flutter.dart';
-import 'core/di/dependency_injection.dart';
-import 'core/router/app_routers.dart';
-import 'ground_scope_app.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Supabase.initialize(
-    url: 'https://bmfsoaduxasmlpkdyzji.supabase.co',
-    anonKey:
-        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJtZnNvYWR1eGFzbWxwa2R5emppIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjI5MzgxMzAsImV4cCI6MjA3ODUxNDEzMH0.Y3pODH_C9L1HGEFYTj_0FNPuQaUeZntlK4ROUVk7-tA',
+    url: AppConstants.supaBaseUr,
+    anonKey: AppConstants.supaBaseKey,
   );
   await setupGetIt();
 
@@ -30,12 +31,7 @@ void main() async {
   } else if (cachedRole == 'admin') {
     initialScreen = const AdminScreen();
   }
-  runApp(
-    GroundScopeApp(
-      appRouter: AppRouter(),
-      initialScreen: initialScreen,
-    ),
-  );
+  runApp(GroundScopeApp(appRouter: AppRouter(), initialScreen: initialScreen));
 }
 
 // flutter run --release --flavor development --target lib/main_development.dart
