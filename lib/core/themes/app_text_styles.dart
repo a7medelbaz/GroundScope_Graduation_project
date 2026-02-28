77d1edde3f4b1984698450871246a1f0117dc580
mustafaelbaz5 <m9stafa05@gmail.com>
2026-02-28 06:26:50 +0200

    Update app TextStyles

--- lib/core/themes/app_text_styles.dart
+++ lib/core/themes/app_text_styles.dart
@@ -1,37 +1,81 @@
 import 'package:flutter/material.dart';
+import 'package:ground_scope/core/utils/spacing.dart';
 
-import 'app_colors.dart';
 import 'app_font_weight.dart';
 
 class AppTextStyles {
-  static TextStyle font30WhiteBold = const TextStyle(
-    fontSize: 30,
-    fontWeight: FontWeightHelper.bold,
-    color: Colors.white,
+  const AppTextStyles._();
+
+  static const TextStyle fontBold = TextStyle(fontWeight: AppFontWeight.bold);
+  static const TextStyle fontSemiBold = TextStyle(
+    fontWeight: AppFontWeight.semiBold,
   );
-  static TextStyle font16WhiteRegular = const TextStyle(
-    fontSize: 16,
-    fontWeight: FontWeightHelper.regular,
-    color: Colors.white,
+  static const TextStyle fontMedium = TextStyle(
+    fontWeight: AppFontWeight.medium,
   );
-  static TextStyle font18WhiteBold = const TextStyle(
-    fontSize: 18,
-    fontWeight: FontWeightHelper.bold,
-    color: Colors.white,
+
+  // Font Size 20
+  static TextStyle font20Bold = TextStyle(
+    fontSize: responsiveFontSize(20),
+    fontWeight: AppFontWeight.bold,
   );
-  static TextStyle font16greyRegular = const TextStyle(
-    fontSize: 16,
-    fontWeight: FontWeightHelper.regular,
-    color: AppColors.grayColor,
+  static TextStyle font20SemiBold = TextStyle(
+    fontSize: responsiveFontSize(20),
+    fontWeight: AppFontWeight.semiBold,
   );
-  static TextStyle font24WhiteBold = const TextStyle(
-    fontSize: 24,
-    fontWeight: FontWeightHelper.bold,
-    color: AppColors.white,
+  static TextStyle font20Regular = TextStyle(
+    fontSize: responsiveFontSize(20),
+    fontWeight: AppFontWeight.regular,
   );
-  static TextStyle font36greyBold = const TextStyle(
-    fontSize: 36,
-    fontWeight: FontWeightHelper.bold,
-    color: AppColors.lightBlue,
+  // Font Size 18
+  static TextStyle font18Bold = TextStyle(
+    fontSize: responsiveFontSize(18),
+    fontWeight: AppFontWeight.bold,
+  );
+  static TextStyle font18SemiBold = TextStyle(
+    fontSize: responsiveFontSize(18),
+    fontWeight: AppFontWeight.semiBold,
+  );
+  static TextStyle font18Medium = TextStyle(
+    fontSize: responsiveFontSize(18),
+    fontWeight: AppFontWeight.medium,
+  );
+
+  // Font Size 16
+  static TextStyle font16Bold = TextStyle(
+    fontSize: responsiveFontSize(16),
+    fontWeight: AppFontWeight.bold,
+  );
+  static TextStyle font16SemiBold = TextStyle(
+    fontSize: responsiveFontSize(16),
+    fontWeight: AppFontWeight.semiBold,
+  );
+  static TextStyle font16Regular = TextStyle(
+    fontSize: responsiveFontSize(16),
+    fontWeight: AppFontWeight.regular,
+  );
+
+  // Font Size 14
+  static TextStyle font14Bold = TextStyle(
+    fontSize: responsiveFontSize(14),
+    fontWeight: AppFontWeight.bold,
+  );
+  static TextStyle font14SemiBold = TextStyle(
+    fontSize: responsiveFontSize(14),
+    fontWeight: AppFontWeight.semiBold,
+  );
+  static TextStyle font14Regular = TextStyle(
+    fontSize: responsiveFontSize(14),
+    fontWeight: AppFontWeight.regular,
+  );
+
+  // Font Size 12
+  static TextStyle font12Regular = TextStyle(
+    fontSize: responsiveFontSize(12),
+    fontWeight: AppFontWeight.regular,
+  );
+  static TextStyle font12SemiBold = TextStyle(
+    fontSize: responsiveFontSize(12),
+    fontWeight: AppFontWeight.semiBold,
   );
 }
