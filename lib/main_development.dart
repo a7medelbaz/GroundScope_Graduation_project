b2ef1f83b3402145c6911547e3c2118a14c110fc
mustafaelbaz5 <m9stafa05@gmail.com>
2026-02-28 05:47:36 +0200

    Update App INit for Setup Theming & Localization and Setup Chicking

--- lib/main_development.dart
+++ lib/main_development.dart
@@ -1,38 +1,31 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
-import 'package:ground_scope/core/config/app_constants.dart';
+import 'package:ground_scope/core/config/app_config.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 import 'core/di/dependency_injection.dart';
-import 'core/onboarding/ui/on_boarding_screen.dart';
 import 'core/router/app_routers.dart';
-import 'core/utils/secure_storage.dart';
 import 'ground_scope_app.dart';
-import 'modules/admin/features/home/admin_screen.dart';
-import 'modules/supervisor/features/home/supervisor_screen.dart';
-import 'modules/worker/core/widgets/worker_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Supabase.initialize(
-    url: AppConstants.supaBaseUr,
-    anonKey: AppConstants.supaBaseKey,
+    url: AppConfig.supaBaseUr,
+    anonKey: AppConfig.supaBaseKey,
   );
-  await setupGetIt();
+  await Future.wait([
+    EasyLocalization.ensureInitialized(),
+    setupHydratedStorage(),
+    setUpDependencies(),
+  ]);
 
-  final storage = SecureStorage();
-  final cachedRole = await storage.getString(key: 'position');
-
-  Widget initialScreen = const OnBoardingScreen();
-
-  if (cachedRole == 'worker') {
-    initialScreen = const WorkerScreen();
-  } else if (cachedRole == 'supervisor') {
-    initialScreen = const SupervisorScreen();
-  } else if (cachedRole == 'admin') {
-    initialScreen = const AdminScreen();
-  }
-  runApp(GroundScopeApp(appRouter: AppRouter(), initialScreen: initialScreen));
+  runApp(
+    EasyLocalization(
+      supportedLocales: const [Locale('en'), Locale('ar')],
+      path: 'assets/translations',
+      startLocale: const Locale('en'),
+      fallbackLocale: const Locale('en'),
+      child: GroundScopeApp(appRouter: AppRouter()),
+    ),
+  );
 }
-
-// flutter run --release --flavor development --target lib/main_development.dart
-// flutter build apk --flavor development --target lib/main_development.dart
