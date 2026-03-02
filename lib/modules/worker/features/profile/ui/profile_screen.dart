b8645eaffeed82c5df872ddf574db1aed7401504
mustafaelbaz5 <m9stafa05@gmail.com>
2026-03-02 05:45:16 +0200

    Set testing buttons In profile Screen

--- lib/modules/worker/features/profile/ui/profile_screen.dart
+++ lib/modules/worker/features/profile/ui/profile_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:ground_scope/core/themes/app_text_styles.dart';
+import 'package:ground_scope/core/utils/functions/app_setting_method.dart';
+import 'package:ground_scope/core/utils/spacing.dart';
+import 'package:ground_scope/core/widgets/custom_text_button.dart';
 
 class ProfileScreen extends StatelessWidget {
   const ProfileScreen({super.key});
@@ -7,8 +9,28 @@ class ProfileScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Center(
-        child: Text('Worker Profile Screen', style: AppTextStyles.font20Bold),
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            verticalSpacing(50),
+            CustomTextButton(
+              text: 'Swith Lan ',
+              onPressed: () {
+                switchLanguage(context);
+              },
+            ),
+            verticalSpacing(50),
+            CustomTextButton(
+              text: 'Swith Theme ',
+              onPressed: () {
+                switchTheme(context);
+              },
+            ),
+            verticalSpacing(50),
+            CustomTextButton(text: 'Logout', onPressed: () {}),
+          ],
+        ),
       ),
     );
   }
