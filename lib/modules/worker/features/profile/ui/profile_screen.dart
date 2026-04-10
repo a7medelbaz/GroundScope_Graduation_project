2796ba0de2c142534c760d140d4f8438ae8d1e2d
mustafaelbaz5 <m9stafa05@gmail.com>
2026-04-10 17:33:32 +0200

    Fix Cubit Realtime Behavier

--- lib/modules/worker/features/profile/ui/profile_screen.dart
+++ lib/modules/worker/features/profile/ui/profile_screen.dart
@@ -1,10 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:ground_scope/core/auth/logic/cubit/auth_cubit.dart';
+import 'package:ground_scope/core/utils/functions/app_setting_method.dart';
+import 'package:ground_scope/core/utils/spacing.dart';
+import 'package:ground_scope/core/widgets/custom_text_button.dart';
 
 class ProfileScreen extends StatelessWidget {
   const ProfileScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const SafeArea(child: Center(child: Text('Profile Screen')));
+    return SafeArea(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          CustomTextButton(
+            text: "LogOut ",
+            onPressed: () {
+              context.read<AuthCubit>().logout();
+            },
+          ),
+          verticalSpacing(50),
+          CustomTextButton(
+            text: "Switch Language",
+            onPressed: () {
+              switchLanguage(context);
+            },
+          ),
+          verticalSpacing(50),
+          CustomTextButton(
+            text: "Switch theme",
+            onPressed: () {
+              switchTheme(context);
+            },
+          ),
+          const Center(child: Text('Profile Screen')),
+        ],
+      ),
+    );
   }
 }
