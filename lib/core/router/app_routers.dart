0040aaf18fde07e3ed76ff8848800f1bf4ff79ee
mustafaelbaz5 <m9stafa05@gmail.com>
2026-04-18 19:35:22 +0200

    Update Router to Pass the task model

--- lib/core/router/app_routers.dart
+++ lib/core/router/app_routers.dart
@@ -1,55 +1,39 @@
-// ignore_for_file: unused_local_variable
 import 'package:flutter/material.dart';
-import 'package:flutter_animate/flutter_animate.dart'; // Add this import
+import 'package:flutter_animate/flutter_animate.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:ground_scope/core/auth/ui/login_screen.dart';
 import 'package:ground_scope/core/di/dependency_injection.dart';
-import 'package:ground_scope/core/onboarding/ui/on_boarding_screen.dart';
 import 'package:ground_scope/core/router/routes.dart';
-import 'package:ground_scope/modules/admin/features/home/admin_screen.dart';
-import 'package:ground_scope/modules/worker/core/main_navigation/ui/worker_scaffold.dart';
+import 'package:ground_scope/core/shared/data/models/task_model.dart';
+import 'package:ground_scope/modules/worker/features/add_report/logic/cubit/add_report_cubit.dart';
 import 'package:ground_scope/modules/worker/features/add_report/ui/add_report_screen.dart';
-import 'package:ground_scope/modules/worker/features/reports/ui/reports_screen.dart';
 import 'package:ground_scope/modules/worker/features/task_details/logic/cubit/task_details_cubit.dart';
 import 'package:ground_scope/modules/worker/features/task_details/ui/task_details_screen.dart';
-import 'package:ground_scope/modules/worker/features/task_info/ui/task_info_screen.dart';
-
-import '../../modules/supervisor/core/main_navigation/supervisor_scaffold.dart';
 
 class AppRouter {
   AppRouter._();
-
-  static Route<dynamic> generateRoute(final RouteSettings settings) {
+  static Route<dynamic> generateRoute(RouteSettings settings) {
     final arguments = settings.arguments as Map<String, dynamic>?;
+
     switch (settings.name) {
-      case Routes.onBoardingScreen:
-        return _buildRoute(const OnBoardingScreen(), settings);
-      case Routes.loginScreen:
-        return _buildRoute(const LoginScreen(), settings);
-      case Routes.workerScaffold:
-        return _buildRoute(const WorkerScaffold(), settings);
       case Routes.taskDetailsScreen:
+        final task = arguments?['task'];
         return _buildRoute(
           BlocProvider(
             create: (context) =>
-                getIt<TaskDetailsCubit>()..initTask(task: arguments['task']),
-            child: TaskDetailsScreen(task: arguments!['task']),
+                getIt<TaskDetailsCubit>()..initTask(task: task),
+            child: TaskDetailsScreen(task: task),
           ),
           settings,
         );
-      case Routes.taskDetailsInfoScreen:
+      case Routes.addReportScreen:
+        final task = arguments?['preSelectedTask'] as TaskModel?;
         return _buildRoute(
-          TaskInfoScreen(task: arguments!['task'], pauses: arguments['pauses']),
+          BlocProvider(
+            create: (context) => getIt<AddReportCubit>(),
+            child: AddReportScreen(preSelectedTask: task),
+          ),
           settings,
         );
-      case Routes.addReportScreen:
-        return _buildRoute(const AddReportScreen(), settings);
-      case Routes.reportsScreen:
-        return _buildRoute(const ReportsScreen(), settings);
-      case Routes.supervisorScaffold:
-        return _buildRoute(const SupervisorScaffold(), settings);
-      case Routes.adminScreen:
-        return _buildRoute(const AdminScreen(), settings);
       default:
         return _buildRoute(
           Scaffold(
